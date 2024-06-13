0d6196e8fb0ef8325b47473e9f44d9393b9da38a
Erick Oliveira <erick.loliveira@hotmail.com>
2024-06-13 17:29:11 -0300

    feat: adding more api gets, creating DTO and auto mapper in the project to facilitate the  DTO connection, transfering from manga object to a more superficial DTO.

---
+++ Domain/DTO/MangaDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace MagnaBackendNet.Domain.DTO
+{
+    public class MangaDTO
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Chapter { get; set; }
+    }
+}
---
+++ Helper/MappingProfiles.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using MagnaBackendNet.domain.models;
+using MagnaBackendNet.Domain.DTO;
+using System.Diagnostics.Metrics;
+
+namespace MagnaBackendNet.Helper
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Manga, MangaDTO>();
+        }
+    }
+}
